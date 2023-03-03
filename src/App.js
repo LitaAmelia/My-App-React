266b7c090a360b6ada8bd6f7ea932e24a0264916
litaamelia <litaamelia126@gmail.com>
2023-03-03 17:32:52 +0700

    event handling

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import React from 'react'
 import Card from './Card';
 
 function App() {
+  const clickHello = () => {
+    console.log('Hello')
+  }
+
   return (
-    <>
-     <Card/>
-    </>
+    <Tombol teksHello={clickHello}/>
   );
 }
+
+function Tombol(props) {
+  return (
+    <button onClick={props.teksHello}>Tombol</button>
+  )
+}
+
 
 export default App;
